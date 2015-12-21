602fed48b77dfb89c97131ec8d7e84c5466ff48a
delsquared <danylo.dubinin@gmail.com>
2015-12-21 21:23:41 +0200

    + interactive test

---
+++ postag-interactive.py
@@ -0,0 +1,20 @@
+import pickle
+import solutionsB
+import nltk
+
+names = 'e_values known_words q_values taglist'.split()
+objects = {}
+for name in names:
+    with open('objects/' + name + '.pkl', 'rb') as object_file:
+        objects[name] = pickle.load(object_file)
+
+input_string = None
+while not input_string in ['q','quit','exit']:
+    if not input_string:
+        input_string = 'Enter an English sentence to tag its tokens with the respective parts of speech, -- this is an example.'
+        print(input_string)
+    else:
+        input_string = raw_input('Sentence > ')
+    sentence = nltk.word_tokenize(input_string)
+    tagged = solutionsB.viterbi([sentence], objects['taglist'], objects['known_words'], objects['q_values'], objects['e_values'])
+    print('Tagged : ' + tagged[0].strip())

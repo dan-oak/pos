ffd74c7df54616d0be777699b7de8c567f87659d
delsquared <danylo.dubinin@gmail.com>
2016-04-13 13:10:18 +0300

    fix rename

--- postag-interactive.py
+++ postag-interactive.py
@@ -2,10 +2,10 @@ import pickle
 import main
 import nltk
 
-names = 'e_values known_words q_values taglist'.split()
+names = 'tagset known_words q_values e_values'.split()
 objects = {}
 for name in names:
-    with open('objects/' + name + '.pkl', 'rb') as object_file:
+    with open('parameters/' + name + '.pkl', 'rb') as object_file:
         objects[name] = pickle.load(object_file)
 
 prompt = 'Sentence > '
@@ -18,5 +18,5 @@ while not input_string in ['q','quit','exit']:
     else:
         input_string = input(prompt)
     sentence = nltk.word_tokenize(input_string)
-    tagged = main.tag_viterbi(sentence, objects['taglist'], objects['known_words'], objects['q_values'], objects['e_values'])
+    tagged = main.tag_viterbi(sentence, objects['tagset'], objects['known_words'], objects['q_values'], objects['e_values'])
     print('Tagged : ' + " ".join(["{0}/{1}".format(*x) for x in tagged]))
